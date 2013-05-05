98f2e6acc33432454b16f0a2e787c49cfb2a5da9
YuFeng <iscode@qq.com>
2013-05-05 17:06:42 +0800

    增加正则命名组捕获支持

--- utils/strings.go
+++ utils/strings.go
@@ -21,6 +21,35 @@ func NewString(i interface{}) Strings {
 	return s
 }
 
+// match regexp with string, and return a named group map
+// Example:
+//   regexp: "(?P<name>[A-Za-z]+)-(?P<age>\\d+)"
+//   string: "CGC-30"
+//   return: map[string]string{ "name":"CGC", "age":"30" }
+func NamedRegexpGroup(str string, reg *regexp.Regexp) (ng map[string]string, matched bool) {
+	rst := reg.FindStringSubmatch(str)
+	//fmt.Printf("%s => %s => %s\n\n", reg, str, rst)
+	if len(rst) < 1 {
+		return
+	}
+	ng = make(map[string]string)
+	lenRst := len(rst)
+	sn := reg.SubexpNames()
+	for k, v := range sn {
+		// SubexpNames contain the none named group,
+		// so must filter v == ""
+		if k == 0 || v == "" {
+			continue
+		}
+		if k+1 > lenRst {
+			break
+		}
+		ng[v] = rst[k]
+	}
+	matched = true
+	return
+}
+
 func (s Strings) String() string {
 	return string(s)
 }
