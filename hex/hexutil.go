efc0cbec733b9f25201dbee129dd77af89fd7320
Damir <iscode@qq.com>
2015-02-09 21:38:08 +0800

    add new method in hex

--- hex/hexutil.go
+++ hex/hexutil.go
@@ -62,3 +62,57 @@ func ByteToTime(data []byte) (t time.Time, err error) {
 	t = time.Date(int(data[6])+ReduceYeer, time.Month(int(data[4])), int(data[3]), int(data[2]), int(data[1]), int(data[0]), time.Now().Nanosecond(), time.UTC)
 	return
 }
+
+//秒 分 时 日 月 星期 年(表示20范围,去掉20)
+func TimeToByteBCD(t time.Time) []byte {
+	data := make([]byte, 7)
+	data[0] = IntToBcd(t.Second())
+	data[1] = IntToBcd(t.Minute())
+	data[2] = IntToBcd(t.Hour())
+	data[3] = IntToBcd(t.Day())
+	data[4] = IntToBcd(int(t.Month()))
+	data[5] = IntToBcd(int(t.Weekday()))      //星期一是0
+	data[6] = IntToBcd(t.Year() - ReduceYeer) //2000年开始
+	return data
+}
+
+//秒 分 时 日 月 星期 年(表示20范围,去掉20)
+func ByteToTimeBCD(data []byte) (t time.Time, err error) {
+	if len(data) != 7 {
+		err = errors.New("data is faild")
+		return
+	}
+	t = time.Date(BcdToInt(data[6])+ReduceYeer, time.Month(BcdToInt(data[4])), BcdToInt(data[3]), BcdToInt(data[2]), BcdToInt(data[1]), BcdToInt(data[0]), time.Now().Nanosecond(), time.UTC)
+	return
+}
+func ToBCD(i uint64) []byte {
+	var bcd []byte
+	for i > 0 {
+		low := i % 10
+		i /= 10
+		hi := i % 10
+		i /= 10
+		var x []byte
+		x = append(x, byte((hi&0xf)<<4)|byte(low&0xf))
+		bcd = append(x, bcd[:]...)
+	}
+	return bcd
+}
+
+func FromBCD(bcd []byte) uint64 {
+	var i uint64 = 0
+	for k := range bcd {
+		r0 := bcd[k] & 0xf
+		r1 := bcd[k] >> 4 & 0xf
+		r := r1*10 + r0
+		i = i*uint64(100) + uint64(r)
+	}
+	return i
+}
+func IntToBcd(value int) byte {
+	return byte((((value / 10) % 10) << 4) | (value % 10))
+}
+
+func BcdToInt(value byte) int {
+	return (int)((value>>4)*10 + (value & 0x0F))
+}
